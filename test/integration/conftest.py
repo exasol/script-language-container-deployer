2f561adab8a6da164f64c1041664acde3ffc538c
Mikhail Beck <mikhail.beck@exasol.com>
2024-05-16 09:24:01 +0100

    Added SLC deployment integration tests (#6)
    * Add documentation build folder to .gitignore
    
    * #5 Added LanguageContainerDeployer integration tests
    
    * #5 Added LanguageContainerDeployer integration tests
    
    * #5 Added LanguageContainerDeployer integration tests
    
    * #5 Added LanguageContainerDeployer integration tests
    
    * #5 Added LanguageContainerDeployer integration tests
    
    * #5 Added LanguageContainerDeployer integration tests
    
    * #5 Added LanguageContainerDeployer integration tests
    
    * #5 Added LanguageContainerDeployer integration tests
    
    * #5 Added language_container_deployer_cli integration tests
    
    * #5 Added language_container_deployer_cli integration tests
    
    * #5 Added language_container_deployer_cli integration tests
    
    * Update test/integration/test_language_container_deployer_cli.py
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Update test/integration/test_language_container_deployer.py
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Update test/integration/test_language_container_deployer_cli.py
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Update test/integration/test_language_container_deployer_cli.py
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Update test/integration/test_language_container_deployer.py
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Update test/integration/test_language_container_deployer_cli.py
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Update test/integration/test_language_container_deployer.py
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Update test/integration/test_language_container_deployer.py
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Update test/integration/test_language_container_deployer.py
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Update test/integration/test_language_container_deployer.py
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * #5 Addressed review issues
    
    * #5 Addressed review issues
    
    ---------
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>

---
+++ test/integration/conftest.py
@@ -0,0 +1,55 @@
+import pytest
+import click
+import requests
+
+from exasol.python_extension_common.deployment.language_container_deployer_cli import (
+    language_container_deployer_main, slc_parameter_formatters, CustomizableParameters)
+
+
+SLC_NAME = "template-Exasol-all-python-3.10_release.tar.gz"
+
+SLC_URL_FORMATTER = ("https://github.com/exasol/script-languages-release/releases/"
+                     "download/{version}/") + SLC_NAME
+
+VERSION = "8.0.0"
+
+
+@pytest.fixture
+def main_func():
+
+    @click.group()
+    def fake_main():
+        pass
+
+    slc_parameter_formatters.set_formatter(CustomizableParameters.container_url, SLC_URL_FORMATTER)
+    slc_parameter_formatters.set_formatter(CustomizableParameters.container_name, SLC_NAME)
+
+    fake_main.add_command(language_container_deployer_main)
+    return fake_main
+
+
+@pytest.fixture(scope='session')
+def container_version() -> str:
+    return VERSION
+
+
+@pytest.fixture(scope='session')
+def container_name() -> str:
+    return SLC_NAME
+
+
+@pytest.fixture(scope='session')
+def container_url(container_version) -> str:
+    return SLC_URL_FORMATTER.format(version=VERSION)
+
+
+@pytest.fixture(scope='session')
+def container_path(tmpdir_factory, container_url, container_name) -> str:
+
+    response = requests.get(container_url, allow_redirects=True)
+    response.raise_for_status()
+    slc_path = tmpdir_factory.mktemp('container').join(container_name)
+    slc_path = str(slc_path)
+    with open(slc_path, 'wb') as f:
+        f.write(response.content)
+    return slc_path
---
+++ test/integration/test_language_container_deployer.py
@@ -0,0 +1,100 @@
+from typing import Callable
+from contextlib import ExitStack
+from pathlib import Path
+
+import pytest
+
+from pyexasol import ExaConnection
+from pytest_itde import config
+from exasol_bucketfs_utils_python.bucketfs_factory import BucketFSFactory
+
+from exasol.python_extension_common.deployment.language_container_deployer import (
+    LanguageContainerDeployer, LanguageActivationLevel)
+
+from test.utils.revert_language_settings import revert_language_settings
+from test.utils.db_utils import (create_schema, assert_udf_running)
+
+TEST_SCHEMA = "PEC_DEPLOYER_TESTS"
+TEST_LANGUAGE_ALIAS = "PYTHON3_PEC_TESTS"
+
+
+def create_container_deployer(language_alias: str,
+                              pyexasol_connection: ExaConnection,
+                              bucketfs_config: config.BucketFs) -> LanguageContainerDeployer:
+    bucket_fs_factory = BucketFSFactory()
+    bucketfs_location = bucket_fs_factory.create_bucketfs_location(
+        url=f"{bucketfs_config.url}/default/container;bfsdefault",
+        user=f"{bucketfs_config.username}",
+        pwd=f"{bucketfs_config.password}",
+        base_path=None)
+    return LanguageContainerDeployer(
+        pyexasol_connection, language_alias, bucketfs_location)
+
+
+def test_language_container_deployer(
+        itde: config.TestConfig,
+        connection_factory: Callable[[config.Exasol], ExaConnection],
+        container_path: str):
+    """
+    Tests the deployment of a container in one call, including the activation at the System level.
+    """
+    with ExitStack() as stack:
+        pyexasol_connection = stack.enter_context(connection_factory(itde.db))
+        stack.enter_context(revert_language_settings(pyexasol_connection))
+        create_schema(pyexasol_connection, TEST_SCHEMA)
+        deployer = create_container_deployer(language_alias=TEST_LANGUAGE_ALIAS,
+                                             pyexasol_connection=pyexasol_connection,
+                                             bucketfs_config=itde.bucketfs)
+        deployer.run(container_file=Path(container_path), alter_system=True, allow_override=True)
+        new_connection = stack.enter_context(connection_factory(itde.db))
+        assert_udf_running(new_connection, TEST_LANGUAGE_ALIAS, TEST_SCHEMA)
+
+
+def test_language_container_deployer_alter_session(
+        itde: config.TestConfig,
+        connection_factory: Callable[[config.Exasol], ExaConnection],
+        container_url: str,
+        container_name: str):
+    """
+    Tests the deployment of a container in two stages - uploading the container
+    followed by activation at the Session level.
+    """
+    with ExitStack() as stack:
+        pyexasol_connection = stack.enter_context(connection_factory(itde.db))
+        stack.enter_context(revert_language_settings(pyexasol_connection))
+        create_schema(pyexasol_connection, TEST_SCHEMA)
+        deployer = create_container_deployer(language_alias=TEST_LANGUAGE_ALIAS,
+                                             pyexasol_connection=pyexasol_connection,
+                                             bucketfs_config=itde.bucketfs)
+        deployer.download_and_run(container_url, container_name, alter_system=False)
+        new_connection = stack.enter_context(connection_factory(itde.db))
+        deployer = create_container_deployer(language_alias=TEST_LANGUAGE_ALIAS,
+                                             pyexasol_connection=new_connection,
+                                             bucketfs_config=itde.bucketfs)
+        deployer.activate_container(container_name, LanguageActivationLevel.Session, True)
+        assert_udf_running(new_connection, TEST_LANGUAGE_ALIAS, TEST_SCHEMA)
+
+
+def test_language_container_deployer_activation_fail(
+        itde: config.TestConfig,
+        connection_factory: Callable[[config.Exasol], ExaConnection],
+        container_path: str,
+        container_name: str):
+    """
+    Tests that an attempt to activate a container using an alias that already exists
+    causes an exception if overriding is disallowed.
+    """
+    with ExitStack() as stack:
+        pyexasol_connection = stack.enter_context(connection_factory(itde.db))
+        stack.enter_context(revert_language_settings(pyexasol_connection))
+        create_schema(pyexasol_connection, TEST_SCHEMA)
+        deployer = create_container_deployer(language_alias=TEST_LANGUAGE_ALIAS,
+                                             pyexasol_connection=pyexasol_connection,
+                                             bucketfs_config=itde.bucketfs)
+        deployer.run(container_file=Path(container_path), alter_system=True, allow_override=True)
+        new_connection = stack.enter_context(connection_factory(itde.db))
+        deployer = create_container_deployer(language_alias=TEST_LANGUAGE_ALIAS,
+                                             pyexasol_connection=new_connection,
+                                             bucketfs_config=itde.bucketfs)
+        with pytest.raises(RuntimeError):
+            deployer.activate_container(container_name, LanguageActivationLevel.System, False)
