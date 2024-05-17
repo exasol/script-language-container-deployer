7cc968090748936cb901ddf83e4465e06a342e65
mibe <mikhail.beck@exasol.com>
2024-05-17 12:34:33 +0100

    #7 Disabled all integration tests but SaaS

--- test/integration/test_language_container_deployer_cli.py
+++ test/integration/test_language_container_deployer_cli.py
@@ -1,6 +1,7 @@
 from typing import Optional, Callable
 from contextlib import ExitStack
 
+import pytest
 from urllib.parse import urlparse
 from click.testing import CliRunner
 from pyexasol import ExaConnection, ExaConnectionFailedError
@@ -58,6 +59,7 @@ def call_language_definition_deployer_cli(func,
     return result
 
 
+@pytest.mark.skip
 def test_language_container_deployer_cli_with_container_file(
         itde: config.TestConfig,
         connection_factory: Callable[[config.Exasol], ExaConnection],
@@ -80,6 +82,7 @@ def test_language_container_deployer_cli_with_container_file(
         assert_udf_running(new_connection, TEST_LANGUAGE_ALIAS, TEST_SCHEMA)
 
 
+@pytest.mark.skip
 def test_language_container_deployer_cli_by_downloading_container(
         itde: config.TestConfig,
         connection_factory: Callable[[config.Exasol], ExaConnection],
@@ -102,6 +105,7 @@ def test_language_container_deployer_cli_by_downloading_container(
         assert_udf_running(new_connection, TEST_LANGUAGE_ALIAS, TEST_SCHEMA)
 
 
+@pytest.mark.skip
 def test_language_container_deployer_cli_with_missing_container_option(
         itde: config.TestConfig,
         connection_factory: Callable[[config.Exasol], ExaConnection],
@@ -118,6 +122,7 @@ def test_language_container_deployer_cli_with_missing_container_option(
         assert isinstance(result.exception, ValueError)
 
 
+@pytest.mark.skip
 def test_language_container_deployer_cli_with_check_cert(
         itde: config.TestConfig,
         connection_factory: Callable[[config.Exasol], ExaConnection],
