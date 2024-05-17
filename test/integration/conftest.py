62c1c3805d76474a6d9925e4394af16934e4c1a0
mibe <mikhail.beck@exasol.com>
2024-05-17 12:12:11 +0100

    #7 Added an integration test for SaaS

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -1,6 +1,18 @@
+import os
 import pytest
 import click
 import requests
+
+import pyexasol
+from exasol.saas.client.api_access import (
+    create_saas_client,
+    timestamp_name,
+    OpenApiAccess,
+)
+from exasol.saas.client.openapi.models import CreateAllowedIP
+from exasol.saas.client.openapi.api.security.add_allowed_ip import sync as add_allowed_ip
+from exasol.saas.client.openapi.api.clusters.list_clusters import sync as list_clusters
+from exasol.saas.client.openapi.api.clusters.get_cluster_connection import sync as get_cluster_connection
 
 from exasol.python_extension_common.deployment.language_container_deployer_cli import (
     language_container_deployer_main, slc_parameter_formatters, CustomizableParameters)
@@ -53,3 +65,65 @@ def container_path(tmpdir_factory, container_url, container_name) -> str:
     with open(slc_path, 'wb') as f:
         f.write(response.content)
     return slc_path
+
+
+def _env(var: str) -> str:
+    result = os.environ.get(var)
+    if result:
+        return result
+    raise RuntimeError(f"Environment variable {var} is empty.")
+
+
+@pytest.fixture(scope="session")
+def saas_host() -> str:
+    return _env("SAAS_HOST")
+
+
+@pytest.fixture(scope="session")
+def saas_token() -> str:
+    return _env("SAAS_PAT")
+
+
+@pytest.fixture(scope="session")
+def saas_account_id() -> str:
+    return _env("SAAS_ACCOUNT_ID")
+
+
+@pytest.fixture(scope="session")
+def api_access(saas_host, saas_token, saas_account_id) -> OpenApiAccess:
+    with create_saas_client(saas_host, saas_token) as client:
+        yield OpenApiAccess(client, saas_account_id)
+
+
+@pytest.fixture(scope="session")
+def operational_saas_database_id(api_access) -> str:
+    database_name = timestamp_name('PEC')
+    with api_access.database(database_name) as db:
+        api_access.wait_until_running(db.id)
+        yield db
+
+
+@pytest.fixture(scope="session")
+def saas_connection_factory(saas_host, saas_token, saas_account_id, operational_saas_database_id):
+
+    with create_saas_client(saas_host, saas_token) as client:
+        ip_rule = CreateAllowedIP(name=timestamp_name('PEC_IP'),
+                                  cidr_ip='0.0.0.0/0')
+        add_allowed_ip(saas_account_id,
+                       client=client,
+                       body=ip_rule)
+        clusters = list_clusters(saas_account_id,
+                                 operational_saas_database_id,
+                                 client=client)
+        cluster_id = next(filter(lambda cl: cl.main_cluster, clusters)).id
+        connections = get_cluster_connection(saas_account_id,
+                                             operational_saas_database_id,
+                                             cluster_id,
+                                             client=client)
+
+        def create_pyexasol_connection(**kwargs) -> pyexasol.ExaConnection:
+            return pyexasol.connect(dsn=f'{connections.dns}:{connections.port}',
+                                    user=connections.db_username,
+                                    password=saas_token,
+                                    **kwargs)
+        yield create_pyexasol_connection
---
+++ test/integration/test_language_container_deployer_saas.py
@@ -0,0 +1,56 @@
+from contextlib import ExitStack
+from pathlib import Path
+
+from pyexasol import ExaConnection
+import exasol.bucketfs as bfs
+
+from exasol.python_extension_common.deployment.language_container_deployer import (
+    LanguageContainerDeployer)
+
+from test.utils.revert_language_settings import revert_language_settings
+from test.utils.db_utils import (create_schema, assert_udf_running)
+
+TEST_SCHEMA = "PEC_DEPLOYER_TESTS"
+TEST_LANGUAGE_ALIAS = "PYTHON3_PEC_TESTS"
+
+
+def create_container_deployer(language_alias: str,
+                              pyexasol_connection: ExaConnection,
+                              url: str,
+                              account_id: str,
+                              database_id: str,
+                              token: str) -> LanguageContainerDeployer:
+
+    bucketfs_path = bfs.path.build_path(backend=bfs.path.StorageBackend.saas,
+                                        url=url,
+                                        account_id=account_id,
+                                        database_id=database_id,
+                                        pat=token,
+                                        path="container")
+    return LanguageContainerDeployer(
+        pyexasol_connection, language_alias, bucketfs_path)
+
+
+def test_language_container_deployer(
+        saas_host,
+        saas_token,
+        saas_account_id,
+        operational_saas_database_id,
+        saas_connection_factory,
+        container_path: str):
+    """
+    Tests the deployment of a container in one call, including the activation at the System level.
+    """
+    with ExitStack() as stack:
+        pyexasol_connection = stack.enter_context(saas_connection_factory(compression=True))
+        stack.enter_context(revert_language_settings(pyexasol_connection))
+        create_schema(pyexasol_connection, TEST_SCHEMA)
+        deployer = create_container_deployer(language_alias=TEST_LANGUAGE_ALIAS,
+                                             pyexasol_connection=pyexasol_connection,
+                                             url=saas_host,
+                                             account_id=saas_account_id,
+                                             database_id=operational_saas_database_id,
+                                             token=saas_token)
+        deployer.run(container_file=Path(container_path), alter_system=True, allow_override=True)
+        new_connection = stack.enter_context(saas_connection_factory(compression=True))
+        assert_udf_running(new_connection, TEST_LANGUAGE_ALIAS, TEST_SCHEMA)
