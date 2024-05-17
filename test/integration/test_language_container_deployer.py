6dd801e37fd73b15b6bdaf9c47d74a8377498c16
mibe <mikhail.beck@exasol.com>
2024-05-17 12:13:21 +0100

    #7 Added an integration test for SaaS

--- test/integration/test_language_container_deployer.py
+++ test/integration/test_language_container_deployer.py
@@ -34,43 +34,7 @@ def create_container_deployer(language_alias: str,
         pyexasol_connection, language_alias, bucketfs_path)
 
 
-def create_container_deployer_saas(language_alias: str,
-                                   pyexasol_connection: ExaConnection,
-                                   url: str,
-                                   account_id: str,
-                                   database_id: str,
-                                   token: str) -> LanguageContainerDeployer:
-
-    bucketfs_path = bfs.path.build_path(backend=bfs.path.StorageBackend.saas,
-                                        url=url,
-                                        account_id=account_id,
-                                        database_id=database_id,
-                                        pat=token,
-                                        path="container")
-    return LanguageContainerDeployer(
-        pyexasol_connection, language_alias, bucketfs_path)
-
-
 def test_language_container_deployer(
-        itde: config.TestConfig,
-        connection_factory: Callable[[config.Exasol], ExaConnection],
-        container_path: str):
-    """
-    Tests the deployment of a container in one call, including the activation at the System level.
-    """
-    with ExitStack() as stack:
-        pyexasol_connection = stack.enter_context(connection_factory(itde.db))
-        stack.enter_context(revert_language_settings(pyexasol_connection))
-        create_schema(pyexasol_connection, TEST_SCHEMA)
-        deployer = create_container_deployer(language_alias=TEST_LANGUAGE_ALIAS,
-                                             pyexasol_connection=pyexasol_connection,
-                                             bucketfs_config=itde.bucketfs)
-        deployer.run(container_file=Path(container_path), alter_system=True, allow_override=True)
-        new_connection = stack.enter_context(connection_factory(itde.db))
-        assert_udf_running(new_connection, TEST_LANGUAGE_ALIAS, TEST_SCHEMA)
-
-
-def test_language_container_deployer_saas(
         itde: config.TestConfig,
         connection_factory: Callable[[config.Exasol], ExaConnection],
         container_path: str):
